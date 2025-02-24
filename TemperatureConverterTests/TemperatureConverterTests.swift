f23ca665d78352a1cf64c28c24f459722cb1742a
Josephine Salamon <jsalamon@lcs.on.ca>
2025-02-24 14:12:18 -0500

    Initial Commit

---
+++ TemperatureConverterTests/TemperatureConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterTests.swift
+//  TemperatureConverterTests
+//
+//  Created by Josephine Salamon on 2025-02-24.
+//
+
+import Testing
+@testable import TemperatureConverter
+
+struct TemperatureConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
